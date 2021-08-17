29b83a075936ed985c58e852e1cbeaf2e41dbb29
rustico <nmbases@protoonmail.com>
2021-08-16 22:40:12 -0300

    Frontend: send constant number of stones

--- frontend/src/views/NewGame.vue
+++ frontend/src/views/NewGame.vue
@@ -6,7 +6,7 @@ export default {
   name: 'NewGame',
   created() {
     localStorage.clear()
-    axios.get(`${config.API_URL}/games/new`)
+    axios.post(`${config.API_URL}/games/new`, {numberOfStones: 6})
     .then((result) => {
       localStorage.apiKey = result.data.apiKey
       localStorage.invitationApiKey = result.data.invitationApiKey
