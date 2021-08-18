dd63d798ff204d242bec81f975e015d42477c6ce
rustico <nmbases@protoonmail.com>
2021-08-18 17:25:27 -0300

    API: refactoring configurations

--- backend/api/src/main/kotlin/com/bol/api/config/SwaggerConfig.kt
+++ backend/api/src/main/kotlin/com/bol/api/config/SwaggerConfig.kt
@@ -1,4 +1,4 @@
-package com.bol.api.swagger
+package com.bol.api.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
--- backend/api/src/main/kotlin/com/bol/api/config/WebSocketConfig.kt
+++ backend/api/src/main/kotlin/com/bol/api/config/WebSocketConfig.kt
@@ -1,4 +1,4 @@
-package com.bol.api.socket
+package com.bol.api.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
