1f67fbe28c7ffbb451ca6d2bdfb6e84db7a85070
rustico <nmbases@protoonmail.com>
2021-08-18 23:30:08 -0300

    API: add GameService tests

--- backend/api/src/test/kotlin/com/bol/api/service/GameServiceTest.kt
+++ backend/api/src/test/kotlin/com/bol/api/service/GameServiceTest.kt
@@ -1,6 +1,8 @@
 package com.bol.api.service
 
 import com.bol.api.extension.toGameResponse
+import com.bol.api.extension.toJoinGameResponse
+import com.bol.api.extension.toNewGameResponse
 import com.bol.api.model.Game
 import com.bol.api.model.GameMove
 import com.bol.api.repository.GameMoveRepository
@@ -63,4 +65,73 @@ internal class GameServiceTest() {
                 gameService.findByUuid(UUID.randomUUID())
             }
         }
+
+    @Test
+    fun `test it should create a new Game`() {
+        val gameRepository = mockk<GameRepository>()
+        val game = Game()
+        every { gameRepository.save(any()) } returns game
+
+        val gameMoveRepository = mockk<GameMoveRepository>()
+        val mancalaService = mockk<MancalaService>()
+        val gameService = GameService(gameRepository, gameMoveRepository, mancalaService)
+
+        val newGameResponse = gameService.create(game.numberOfStones)
+        val expectedNewGameResponse = game.toNewGameResponse()
+        assertEquals(expectedNewGameResponse.uuid, newGameResponse.uuid)
+        assertEquals(expectedNewGameResponse.apiKey, newGameResponse.apiKey)
+        assertEquals(expectedNewGameResponse.invitationApiKey, newGameResponse.invitationApiKey)
+        assertEquals(expectedNewGameResponse.createdAt, newGameResponse.createdAt)
+        assertEquals(expectedNewGameResponse.updatedAt, newGameResponse.updatedAt)
+    }
+
+    @Test
+    fun `test it should join a created Game with a valid invitation api key`() {
+        val gameRepository = mockk<GameRepository>()
+        val game = Game()
+        every { gameRepository.findByUuid(game.uuid) } returns game
+        every { gameRepository.save(game) } returns game
+
+        val gameMoveRepository = mockk<GameMoveRepository>()
+        val mancalaService = mockk<MancalaService>()
+        val gameService = GameService(gameRepository, gameMoveRepository, mancalaService)
+
+        val joinGameResponse = gameService.joinGame(game.uuid, game.invitationApiKey!!)
+        val expectedJoinGameResponse = game.toJoinGameResponse()
+
+        assertEquals(game.invitationApiKey, null)
+        assertEquals(expectedJoinGameResponse.uuid, joinGameResponse.uuid)
+        assertEquals(expectedJoinGameResponse.apiKey, joinGameResponse.apiKey)
+    }
+
+    @Test
+    fun `test it should throw GameAlreadyStartedException when game has already started`() {
+        val gameRepository = mockk<GameRepository>()
+        val game = Game()
+        game.invitationApiKey = null
+        every { gameRepository.findByUuid(game.uuid) } returns game
+
+        val gameMoveRepository = mockk<GameMoveRepository>()
+        val mancalaService = mockk<MancalaService>()
+        val gameService = GameService(gameRepository, gameMoveRepository, mancalaService)
+
+        assertThrows(GameAlreadyStartedException::class.java) {
+            gameService.joinGame(game.uuid, UUID.randomUUID())
+        }
+    }
+
+    @Test
+    fun `test it should throw InvalidAPIKeyException when game has already started`() {
+        val gameRepository = mockk<GameRepository>()
+        val game = Game()
+        every { gameRepository.findByUuid(game.uuid) } returns game
+
+        val gameMoveRepository = mockk<GameMoveRepository>()
+        val mancalaService = mockk<MancalaService>()
+        val gameService = GameService(gameRepository, gameMoveRepository, mancalaService)
+
+        assertThrows(InvalidAPIKeyException::class.java) {
+            gameService.joinGame(game.uuid, UUID.randomUUID())
+        }
+    }
 }
